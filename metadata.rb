75860b947b0089aa7442b7adeff78ae3e3daa437
Cobus Bernard <cobusbernard@gmail.com>
2016-01-28 18:46:25 +0200

    Forgot to bump the version number.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@cobus.io'
 license          'MIT'
 description      'Installs/Configures chef-wrapper-consul'
 long_description 'Installs/Configures chef-wrapper-consul'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'consul', '~> 1.3.1'
